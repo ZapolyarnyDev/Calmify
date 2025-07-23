aceae581be6fb27efe7bfba5526b8a39319ff47a
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-07-23 17:47:43 +0300

    chore(api-gateway): basic setup and dependency installation

---
+++ api-gateway/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    java
+    war
+    id("org.springframework.boot") version "3.4.7"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "io.github.zapolyarnydev"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+extra["springCloudVersion"] = "2024.0.0"
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.bootJar {
+    archiveBaseName.set("api-gateway")
+    archiveVersion.set("1.0")
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+}
