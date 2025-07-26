08334058b56122556fbd4ff30b44624375089719
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-07-26 20:44:02 +0300

    test: add GitHub Actions workflows for unit tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,30 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
+configure(listOf(project(":auth-service"))) {
+    tasks.register<Test>("unitTest") {
+        description = "Runs unit tests"
+        group = "verification"
+
+        testClassesDirs = sourceSets.test.get().output.classesDirs
+        classpath = sourceSets.test.get().runtimeClasspath
+
+        useJUnitPlatform {
+            includeTags("unit")
+        }
+    }
+}
+
+tasks.register("unitTestAll") {
+    group = "verification"
+    description = "Runs unit tests in all subprojects"
+    dependsOn(
+        listOf(project(":auth-service"))
+            .map { it.tasks.named("unitTest") }
+    )
+}
+
+
 tasks.test {
     useJUnitPlatform()
 }
