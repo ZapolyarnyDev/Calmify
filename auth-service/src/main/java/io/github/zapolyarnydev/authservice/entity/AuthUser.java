a4a406386da881a1769ae93a6b54138d25f887b2
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-07-27 12:25:13 +0300

    feature: add AuthUser entity and repository

---
+++ auth-service/src/main/java/io/github/zapolyarnydev/authservice/entity/AuthUser.java
@@ -0,0 +1,40 @@
+package io.github.zapolyarnydev.authservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "auth-users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class AuthUser {
+
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id = UUID.randomUUID();
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, updatable = false)
+    private Instant registeredAt;
+
+    public AuthUser(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @PrePersist
+    public void prePersist(){
+        this.registeredAt = Instant.now();
+    }
+}
