7998fbf5e93a390eb9cf966cff2c2bd602652a7c
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-07-30 20:49:30 +0300

    feat(commons): add avro scheme for user created event

---
+++ calmify-commons/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    java
+    id("com.github.davidmc24.gradle.plugin.avro") version "1.9.1"
+}
+
+group = "io.github.zapolyarnydev"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.apache.avro:avro:1.11.4")
+}
+
+avro {
+    fieldVisibility.set("PRIVATE")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
