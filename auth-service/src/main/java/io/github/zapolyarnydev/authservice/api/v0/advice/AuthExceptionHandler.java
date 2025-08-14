9b83ea18a9b585a945acc8022cb5fecce5c9e2a3
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-08-14 23:33:54 +0300

    refactor(auth-service): change http status code numbers to HttpStatus enum in exception handlers  [skip ci]

--- auth-service/src/main/java/io/github/zapolyarnydev/authservice/api/v0/advice/AuthExceptionHandler.java
+++ auth-service/src/main/java/io/github/zapolyarnydev/authservice/api/v0/advice/AuthExceptionHandler.java
@@ -5,6 +5,7 @@ import io.github.zapolyarnydev.authservice.exception.EmailNotFoundException;
 import io.github.zapolyarnydev.authservice.exception.InvalidCredentialsException;
 import io.github.zapolyarnydev.commons.api.ApiResponse;
 import io.github.zapolyarnydev.commons.api.ApiStatus;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,19 +16,19 @@ public class AuthExceptionHandler {
     @ExceptionHandler(EmailAlreadyUsedException.class)
     public ResponseEntity<ApiResponse<?>> emailUsed(EmailAlreadyUsedException e) {
         var response = ApiResponse.fail(e.getMessage());
-        return ResponseEntity.status(409).body(response);
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
     }
 
     @ExceptionHandler(EmailNotFoundException.class)
     public ResponseEntity<ApiResponse<?>> emailNotFound(EmailNotFoundException e) {
         var response = ApiResponse.fail(e.getMessage());
-        return ResponseEntity.status(404).body(response);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
     @ExceptionHandler(InvalidCredentialsException.class)
     public ResponseEntity<ApiResponse<?>> invalidCredentials(InvalidCredentialsException e) {
         var response = ApiResponse.fail(e.getMessage());
-        return ResponseEntity.status(409).body(response);
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
     }
 
 }
--- auth-service/src/main/java/io/github/zapolyarnydev/authservice/api/v0/advice/TokenExceptionHandler.java
+++ auth-service/src/main/java/io/github/zapolyarnydev/authservice/api/v0/advice/TokenExceptionHandler.java
@@ -4,6 +4,7 @@ import io.github.zapolyarnydev.commons.api.ApiResponse;
 import io.github.zapolyarnydev.commons.api.ApiStatus;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,18 +15,18 @@ public class TokenExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ApiResponse<?>> illegalArgument(IllegalArgumentException e) {
         var response = ApiResponse.fail(e.getMessage());
-        return ResponseEntity.status(400).body(response);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
     @ExceptionHandler(ExpiredJwtException.class)
     public ResponseEntity<ApiResponse<?>> expiredToken(ExpiredJwtException ignored) {
         var response = ApiResponse.fail("Token expired");
-        return ResponseEntity.status(401).body(response);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
     }
 
     @ExceptionHandler(MalformedJwtException.class)
     public ResponseEntity<ApiResponse<?>> malformedToken(MalformedJwtException ignored) {
         var response = ApiResponse.fail("Malformed token");
-        return ResponseEntity.status(400).body(response);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
