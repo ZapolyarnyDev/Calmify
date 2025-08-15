1e058ed6c502f40a4f6c492718829cbbe1a9f0e6
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-08-15 10:18:28 +0300

    feat: add annotation and exists method to user repository[skip ci]

--- user-service/src/main/java/io/github/zapolyarnydev/userservice/repository/UserRepository.java
+++ user-service/src/main/java/io/github/zapolyarnydev/userservice/repository/UserRepository.java
@@ -2,8 +2,12 @@ package io.github.zapolyarnydev.userservice.repository;
 
 import io.github.zapolyarnydev.userservice.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+    boolean existsByHandle(String handle);
 }
