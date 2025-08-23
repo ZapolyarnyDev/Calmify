3b2b5c959c1aa9f8089ad958711215c6e25a7515
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-08-23 19:41:35 +0300

    chore(engagement-service): basic setup and dependency installation [skip ci]

---
+++ engagement-service/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    java
+    war
+    id("org.springframework.boot") version "3.4.7"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "io.github.zapolyarnydev"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    implementation(project(":calmify-commons"))
+
+    compileOnly("jakarta.validation:jakarta.validation-api:3.0.2")
+    implementation("org.hibernate.validator:hibernate-validator:8.0.1.Final")
+
+    implementation("org.springframework.kafka:spring-kafka")
+
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    compileOnly("org.mapstruct:mapstruct:1.5.5.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    runtimeOnly("org.postgresql:postgresql")
+}
+
+extra["springCloudVersion"] = "2024.0.0"
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.bootJar {
+    archiveFileName.set("app.jar")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
