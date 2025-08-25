d2429c36c03c6c6177eafd12f65c6409ce657715
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-08-25 15:22:45 +0300

    refactor(auth/exception-handling): remove boilerplate handlers [skip ci]

--- auth-service/src/main/java/io/github/zapolyarnydev/authservice/api/v0/advice/AuthExceptionHandler.java
+++ auth-service/src/main/java/io/github/zapolyarnydev/authservice/api/v0/advice/AuthExceptionHandler.java
@@ -3,7 +3,6 @@ package io.github.zapolyarnydev.authservice.api.v0.advice;
 import io.github.zapolyarnydev.authservice.exception.EmailNotFoundException;
 import io.github.zapolyarnydev.authservice.exception.InvalidCredentialsException;
 import io.github.zapolyarnydev.commons.api.ApiResponse;
-import io.github.zapolyarnydev.commons.exception.EmailAlreadyUsedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,22 +11,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class AuthExceptionHandler {
 
-    @ExceptionHandler(EmailAlreadyUsedException.class)
-    public ResponseEntity<ApiResponse<?>> emailUsed(EmailAlreadyUsedException e) {
-        var response = ApiResponse.fail(e.getMessage());
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
-    }
-
     @ExceptionHandler(EmailNotFoundException.class)
     public ResponseEntity<ApiResponse<?>> emailNotFound(EmailNotFoundException e) {
         var response = ApiResponse.fail(e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
-    @ExceptionHandler(InvalidCredentialsException.class)
-    public ResponseEntity<ApiResponse<?>> invalidCredentials(InvalidCredentialsException e) {
-        var response = ApiResponse.fail(e.getMessage());
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+    @ExceptionHandler({
+            InvalidCredentialsException.class,
+            EmailNotFoundException.class
+    })
+    public ResponseEntity<ApiResponse<?>> wrongCredentials(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(ApiResponse.fail(e.getMessage()));
     }
 
 }
