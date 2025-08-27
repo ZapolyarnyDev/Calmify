f966c18ad5f7801a718061f2d6a157311bb57aee
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-08-27 21:18:24 +0300

    feat(video-service/domain): add VideoRate and CommentRate interfaces [skip ci]

---
+++ engagement-service/src/main/java/io/github/zapolyarnydev/engagement/domain/rate/CommentRate.java
@@ -0,0 +1,8 @@
+package io.github.zapolyarnydev.engagement.domain.rate;
+
+import io.github.zapolyarnydev.engagement.domain.Rate;
+
+import java.util.UUID;
+
+public interface CommentRate extends Rate<Long, Long, UUID> {
+}
---
+++ engagement-service/src/main/java/io/github/zapolyarnydev/engagement/domain/rate/VideoRate.java
@@ -0,0 +1,8 @@
+package io.github.zapolyarnydev.engagement.domain.rate;
+
+import io.github.zapolyarnydev.engagement.domain.Rate;
+
+import java.util.UUID;
+
+public interface VideoRate extends Rate<Long, UUID, UUID> {
+}
