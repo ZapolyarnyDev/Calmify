0e1d76141b06d44308a8600548238d23f1a32fb8
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-09-29 18:48:15 +0300

    feat(k8s): add deployment, service and configmap for api-gateway [skip ci]

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
